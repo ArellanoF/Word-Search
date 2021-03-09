aab49e0d6bd7d3141f4b6916908d8aab8f188f82
hesquerdo <hesquerdo@uoc.edu>
2021-03-09 18:54:58 +0100

    Spring boot base

---
+++ src/main/java/com/sopa/app/SopaLetras1Application.java
@@ -0,0 +1,13 @@
+package com.sopa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopaLetras1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopaLetras1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sopa/app/SopaLetras1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopaLetras1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
