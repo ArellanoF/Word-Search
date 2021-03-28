aea2b23a375dcab8adfdf10abf033abb2242362c
hesquerdo <hesquerdo@uoc.edu>
2021-03-28 12:06:03 +0200

    Default Home to login

--- src/main/java/com/sopa/app/HomeController.java
+++ src/main/java/com/sopa/app/HomeController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+	 @RequestMapping("/")
+	    public String defaultHome() {         
+	        return "login";
+	    }
 	 @RequestMapping("/login")
 	    public String login() {         
 	        return "login";
